582bd06914efe3024d9f50b9d4869d1b0e8b319d
Lando B <jrolandobernal@gmail.com>
2016-02-19 16:03:01 -0600

    Added some practical jQuery code to Hide and Show our Sidebar.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -2,20 +2,33 @@
 
 (function () {
 
-    var ele = $("#username");
-    ele.text("Bruce Wayne");
+    //var ele = $("#username");
+    //ele.text("Bruce Wayne");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style["background-color"] = "#888;"
-    });
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style["background-color"] = "#888;"
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Menu");
+        } else {
+            $(this).text("Hide Menu");
+        }
     });
+    
+
 })();
