8d0e6439a7e0c3943381e6b5398013fb4a24c4d0
Lando B <jrolandobernal@gmail.com>
2016-02-19 12:50:19 -0600

    Tried some JavaScript Events on our page.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 
 var ele = document.getElementById("username");
 ele.innerHTML = "Bruce Wayne";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style["background-color"] = "#888;"
+};
+main.onmouseleave = function () {
+    main.style["background-color"] = "";
+};
