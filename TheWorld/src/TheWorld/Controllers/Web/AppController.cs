dea59a0ca505f8d6dc0b381d7dba3c1a58bab93c
Lando B <jrolandobernal@gmail.com>
2016-02-25 12:18:32 -0600

    Added first controller. AppController. And added first view. Index.cshtml.

---
+++ TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+         
+        }
+    }
+}
