89373edafcd854832f6a99f3769b529c27c28084
Lando B <jrolandobernal@gmail.com>
2016-03-14 13:11:43 -0500

    Added a Service

--- TheWorld/src/TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -3,16 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Services;
 using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
         public IActionResult Index()
         {
             return View();
-         
         }
 
         public IActionResult About()
@@ -28,6 +34,10 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            _mailService.SendMail("",
+                "",
+                $"Contact Page from {model.Name} ({model.Email}",
+                model.Message);
             return View();
         }
 
--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using TheWorld.Services;
 
 namespace TheWorld
 {
@@ -16,6 +17,13 @@ namespace TheWorld
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+
+#if DEBUG
+            services.AddScoped<IMailService, DebugMailService>();
+#else
+            services.AddScoped<IMailService, RealMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
