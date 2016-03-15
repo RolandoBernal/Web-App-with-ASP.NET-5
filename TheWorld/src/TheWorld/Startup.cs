45164aae127dd626d51a1a48e0ae3a63f8cd74bf
Lando B <jrolandobernal@gmail.com>
2016-03-15 14:32:35 -0500

    Finished the Contact Form. Is Sending Email now. All validation works.

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -5,13 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TheWorld.Services;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace TheWorld
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
