3c82988e10c6e2651aeb0514a956ab99f4fd0169
Lando B <jrolandobernal@gmail.com>
2016-03-15 15:22:15 -0500

    Added Bootstrap.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -20,13 +20,16 @@
     //});
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
 
     $("#sidebarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Menu");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
         } else {
-            $(this).text("Hide Menu");
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
         }
     });
     
