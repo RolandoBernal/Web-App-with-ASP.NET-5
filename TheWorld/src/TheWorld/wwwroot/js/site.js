6708bfd28d572e50c50fa44d788e0b4e7270accc
Lando B (Rolando Bernal) <jrolandobernal@gmail.com>
2016-07-22 13:32:51 -0500

    Working on this app again.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -2,22 +2,6 @@
 
 (function () {
 
-    //var ele = $("#username");
-    //ele.text("Bruce Wayne");
-
-    //var main = $("#main");
-    //main.on("mouseenter", function () {
-    //    main.style["background-color"] = "#888;"
-    //});
-    //main.on("mouseleave", function () {
-    //    main.style = "";
-    //});
-
-    //var menuItems = $("ul.menu li a");
-    //menuItems.on("click", function () {
-    //    var me = $(this);
-    //    alert(me.text());
-    //});
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
     var $icon = $("#sidebarToggle i.fa");
